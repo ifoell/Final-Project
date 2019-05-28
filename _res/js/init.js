9c4ba353273208cbf2f912911bde16a252ee62b5
syaiful <syaiful.furiqi@bni.co.id>
2019-05-28 16:10:25 +0700

    try 1

---
+++ _res/js/init.js
@@ -0,0 +1,13 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.slider').slider();
+    $('.carousel').carousel();
+    $('.modal').modal();
+    $('.collapsible').collapsible();
+    $(".dropdown-trigger").dropdown();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
